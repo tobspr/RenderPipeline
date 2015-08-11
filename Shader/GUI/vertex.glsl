778b0c9d0b0bdcdc9f3c6ffbee509624b7e0895d
tobspr <tobias.springer1@googlemail.com>
2015-08-11 22:06:00 +0200

    Smaller fixes

--- Shader/GUI/vertex.glsl
+++ Shader/GUI/vertex.glsl
@@ -4,6 +4,7 @@ uniform mat4 p3d_ModelViewProjectionMatrix;
 in vec4 p3d_Vertex;
 in vec2 p3d_MultiTexCoord0;
 in vec4 p3d_Color;
+in vec4 color;
 uniform vec4 p3d_ColorScale;
 
 out vec2 texcoord;
@@ -13,6 +14,6 @@ void main() {
     
     gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
     texcoord = p3d_MultiTexCoord0;
-    colorMultiply = p3d_Color * p3d_ColorScale;
+    colorMultiply = color * p3d_ColorScale;
 
 }
