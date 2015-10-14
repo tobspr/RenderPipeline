bf9db3ce41fd507bfa6aeb844734c9f2adbe7cd0
tobspr <tobias.springer1@googlemail.com>
2015-10-14 10:14:20 +0200

    Smaller changes to the pipeline debugger interface

--- Code/Util/Generic.py
+++ Code/Util/Generic.py
@@ -4,10 +4,12 @@
 import hashlib
 
 
-def rgb_from_string(text, min_brightness=0.5):
+def rgb_from_string(text, min_brightness=0.4):
     """ Creates a rgb color from a given string """
     ohash = hashlib.md5(text).hexdigest()
-    r, g, b = int(ohash[0:2], 16), int(ohash[8:10], 16), int(ohash[14:16], 16)
+    r, g, b = int(ohash[0:2], 16), int(ohash[4:6], 16), int(ohash[10:12], 16)
+    # Red doesn't look that good, so scale it down
+    r *= 0.92
     neg_inf = 1.0 - min_brightness
     return (min_brightness + r / 255.0 * neg_inf,
             min_brightness + g / 255.0 * neg_inf,
