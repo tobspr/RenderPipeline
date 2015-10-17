8c2f0398bebc213a03bed7d617c819e5d8e6d6fb
tobspr <tobias.springer1@googlemail.com>
2015-10-17 14:54:19 +0200

    Fix linux error in the finalize script

--- Native/Scripts/finalize.py
+++ Native/Scripts/finalize.py
@@ -2,20 +2,28 @@
 
 # This script just copies the generated .pyd file to the current directory.
 
+import platform
 from shutil import copyfile
 from os.path import isfile
-
 source_file = None
 
-possible_files = [
-    "Windows/Release/RSNative.pyd",
-    "Windows/x64/Release/RSNative.pyd",
-]
+
+if platform.system() == "Windows":
+    possible_files = [
+        "Windows/Release/RSNative.pyd",
+        "Windows/x64/Release/RSNative.pyd",
+    ]
+    target_location = "RSNative.pyd"
+elif platform.system() == "Linux":
+    possible_files = [
+        "Linux/RSNative.so"
+    ]
+    target_location = "RSNative.so"
 
 for file in possible_files:
     if isfile(file):
         source_file = file
 
 if source_file:
-    copyfile(source_file, "RSNative.pyd")
+    copyfile(source_file, target_file)
 
