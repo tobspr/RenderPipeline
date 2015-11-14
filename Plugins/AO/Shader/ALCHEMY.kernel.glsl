08428740559025c2cc9a4963e7705bccb9cae711
tobspr <tobias.springer1@googlemail.com>
2015-11-14 20:16:14 +0100

    Improve ALCHEMY ao with bent normal, improve SSVO

--- Plugins/AO/Shader/ALCHEMY.kernel.glsl
+++ Plugins/AO/Shader/ALCHEMY.kernel.glsl
@@ -10,15 +10,17 @@ to approximate AO.
 
 
 const int num_samples = GET_SETTING(AO, alchemy_num_samples);
-float max_dist = GET_SETTING(AO, alchemy_max_distance) ;
+float max_dist = GET_SETTING(AO, alchemy_max_distance);
 float accum = 0.0;
 float accum_count = 0;
+
+vec3 bent_normal = vec3(0);
 
 for (int i = 0; i < num_samples; ++i) {
 
     // Get random texcoord offset
     vec2 offset = poisson_disk_2D_32[i] + noise_vec.xy * 0.2;
-    vec2 offcoord = texcoord + offset * pixel_size * sample_radius * kernel_scale;
+    vec2 offcoord = texcoord + offset * pixel_size * sample_radius * kernel_scale * 0.1;
 
     // Get view position at that offset
     vec3 off_pos = get_view_pos_at(offcoord);
@@ -26,18 +28,28 @@ for (int i = 0; i < num_samples; ++i) {
     // Get the vector s-p to that sample position
     vec3 sample_vec = normalize(off_pos - pixel_view_pos);
 
-
     // Check if the distance matches, discard matches which are too far away
     float dist = distance(off_pos, pixel_view_pos) / max_dist;
     if (dist < 1.0) {
 
         // Weight sample by the angle
-        accum += max(0, dot(pixel_normal, sample_vec)) / dist;
+        accum += max(0, dot(pixel_view_normal, sample_vec)) / dist;
+
+        // Update bent normal
     }
+
+    bent_normal += normalize(-sample_vec);
 
 }
 
+// Normalize
+bent_normal /= max(1.0, length(bent_normal));
+// bent_normal = viewNormalToWorld(bent_normal);
+
+// TODO: Bent normal is buggy right now. Using world space normal instead
+bent_normal = pixel_world_normal;
+
 // Normalize values
 accum /= num_samples;
-accum *= 0.5;
-result = vec4(1 - saturate(accum));
+accum *= 0.05;
+result = vec4(bent_normal, 1 - saturate(accum));
--- Plugins/AO/Shader/SSVO.kernel.glsl
+++ Plugins/AO/Shader/SSVO.kernel.glsl
@@ -13,39 +13,54 @@ const int num_samples = GET_SETTING(AO, ssvo_sample_count);
 
 float accum = 0.0;
 float accum_count = 0;
-vec2 sphere_radius = GET_SETTING(AO, ssvo_sphere_radius) / kernel_scale * pixel_size;
+vec2 sphere_radius = GET_SETTING(AO, ssvo_sphere_radius) * pixel_size;
 
 float pixel_linz = getLinearZFromZ(pixel_depth);
-float max_depth_diff = 10.0 / kernel_scale;
+float max_depth_diff = GET_SETTING(AO, ssvo_max_distance) / kernel_scale;
 
 
 for (int i = 0; i < num_samples; ++i) {
 
 
     vec2 offset = poisson_disk_2D_32[i];
-    offset += noise_vec.xy * 0.1;
-    vec2 offcoord = texcoord + offset * sphere_radius * 5.0;
+    offset += noise_vec.xy * 1.0;
+    vec2 offc = offset * sphere_radius * 6.0 * kernel_scale;
 
-    float sphere_height = sqrt(  1 - dot(offset, offset) );
+    vec2 offcoord_a = texcoord + offc;
+    vec2 offcoord_b = texcoord - offc;
+
+    float sphere_height = ( 1 - dot(offset, offset) );
+
+    float depth_a = get_depth_at(offcoord_a);
+    float depth_b = get_depth_at(offcoord_b);
+
+    float depth_linz_a = getLinearZFromZ(depth_a);
+    float depth_linz_b = getLinearZFromZ(depth_b);
+
+    float diff_a = (pixel_linz - depth_linz_a) / max_depth_diff;
+    float diff_b = (pixel_linz - depth_linz_b) / max_depth_diff;
+
+    float volume_a = (sphere_height - diff_a) / (2.0 * sphere_height);
+    float volume_b = (sphere_height - diff_b) / (2.0 * sphere_height);
+
+    bool valid_a = diff_a <= sphere_height && diff_a >= -sphere_height;
+    bool valid_b = diff_b <= sphere_height && diff_b >= -sphere_height;
 
 
-    float depth_a = get_depth_at(offcoord);
-    float depth_linz = getLinearZFromZ(depth_a);
-
-    float diff = (depth_linz - pixel_linz) / max_depth_diff;
-
-
-    if (diff < sphere_height && diff > -sphere_height) {
-
-        float percentage = (diff+sphere_height) / (2.0*sphere_height);
-        accum += percentage;
-
-        accum_count += 1.0;
+    if (valid_a || valid_b) {
+        accum += valid_a ? volume_a : 1 - volume_b;  
+        accum += valid_b ? volume_b : 1 - volume_a;  
+    } else {
+        accum += 1.0;
     }
 
 }
-// accum /= num_samples;
-accum /= max(1.0, accum_count);
+
+// No bent normal supported yet, use pixel normal
+vec3 bent_normal = pixel_world_normal;
+
+accum /= num_samples;
+accum = pow(accum, 3.8);
 accum = saturate(accum);
-result = vec4(accum);
+result = vec4(bent_normal, accum);
 
