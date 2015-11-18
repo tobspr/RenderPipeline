8df7436c4cbc8ab81b5f039d821a5c48ee4b0dd0
tobspr <tobias.springer1@googlemail.com>
2015-11-18 22:18:21 +0100

    Move curve implementation, smaller improvements

--- Toolkit/DayTimeEditor/CurveWidget.py
+++ Toolkit/DayTimeEditor/CurveWidget.py
@@ -2,120 +2,11 @@
 from __future__ import print_function
 
 import math
-import sys
-from random import random
 
 import PyQt4.QtCore as QtCore 
 import PyQt4.QtGui as QtGui
 
-from panda3d.core import NurbsCurve, Vec3, Notify, HermiteCurve, CurveFitter
-
-
-class Curve:
-
-    """ Interface to the NURBS curve which also manages connecting the end of the
-    curve with the beginning """
-
-    def __init__(self):
-        self._curve = None
-
-        # Append some points to the border, to make sure the curve matches at
-        # the edges
-        self._border_points = 1
-
-        # Curve color, used for displaying the curve
-        self._color = (0, 0, 0)
-
-        # Control points, those are some demo values
-        self._cv_points = [
-            [0.1, 0.5 + 0.4 * random()],
-            [0.3, 0.2 + 0.2 * random()],
-            [0.5, 0.4 + 0.3 * random()],
-            [0.75, 0 + 0.3 * random()],
-            # [1.0, 1]
-        ]
-
-        # Build the curve
-        self.build_curve()
-
-    def get_cv_points(self):
-        """ Returns a list of all controll points """
-        return self._cv_points
-
-    def set_color(self, r, g, b):
-        """ Sets the display color of the curve """
-        self._color = (r, g, b)
-
-    def get_color(self):
-        """ Returns the display color of the curve """
-        return self._color
-
-    def remove_cv(self, index):
-        """ Attempts to remove the cv at the given index, does nothing if only
-        one control point is left """
-        if len(self._cv_points) > 1:
-            del self._cv_points[index]
-        self.build_curve()
-
-    def _get_cv_tangent(self, index):
-        """ Returns the tangent of the nth point """
-        pb = self._cv_points[(index-1) % len(self._cv_points)]
-        pm = self._cv_points[index]
-        pa = self._cv_points[(index+1) % len(self._cv_points)]
-
-        get_diff = lambda p1, p2: Vec3( p1[0]-p2[0], p1[1]-p2[1], 0)
-
-        tangent_left = get_diff(pm, pb)
-        tangent_right = get_diff(pa, pm)
-
-        tangent_avg = (tangent_left + tangent_right) * 0.5
-        return tangent_avg
-
-    def build_curve(self):
-        """ Rebuilds the curve based on the controll point values """
-
-        sorted_points = sorted(self._cv_points, key=lambda v: v[0])
-        first_point = sorted_points[0]
-
-        fitter = CurveFitter()
-
-        # Duplicate curve at the beginning
-        for i in range(self._border_points):
-            end_point = self._cv_points[ (-i + self._border_points - 1) % len(self._cv_points) ]
-            end_point = first_point
-            fitter.add_xyz(0.0, Vec3(0, end_point[1], 0))
-            
-        # Append the actual points
-        for point in self._cv_points:
-            # Clamp point x position to avoid artifacts at the beginning
-            point_t = max(0.01, point[0])
-            fitter.add_xyz(point_t, Vec3(point_t, point[1], 0))
-
-        # Duplicate curve at the end
-        for i in range(self._border_points):
-            start_point = self._cv_points[i % len(self._cv_points)]
-            start_point = first_point
-            fitter.add_xyz(1.0, Vec3(1, start_point[1], 0))
-
-        fitter.sort_points()
-        fitter.compute_tangents(1.0)
-        self._curve = fitter.make_hermite()
-
-    def set_cv_value(self, index, x_value, y_value):
-        """ Updates the cv point at the given index """
-        self._cv_points[index] = [x_value, y_value]
-
-    def get_curve_scale(self):
-        """ Returns the scale of the curve """
-        return self._curve.get_max_t()
-
-    def get_value(self, offset): 
-        """ Returns the value on the curve ranging whereas the offset should be
-        from 0 to 1 (0 denotes the start of the curve). The returned value will
-        be a value from 0 to 1 as well. """
-        point = Vec3(0)
-        self._curve.evaluate_xyz(offset, point)
-        return point.y
+from Code.DayTime.Curve import Curve
 
 class CurveWidget(QtGui.QWidget):
 
@@ -170,18 +61,32 @@ class CurveWidget(QtGui.QWidget):
         self._selected_point = None
         mouse_pos = QMouseEvent.pos()
         mouse_x = mouse_pos.x() - self._legend_border
-        mouse_y = mouse_pos.y()
+        mouse_y = mouse_pos.y() - self._bar_h
 
         for index, curve in enumerate(self.curves):
+
+            # Check for clicks on control points
             for cv_index, (x, y) in enumerate(curve.get_cv_points()):
                 point_x = self._get_x_value_for(x)
-                point_y = self._get_y_value_for(y)
+                point_y = self._get_y_value_for(y) - self._bar_h
                 if abs(point_x - mouse_x) < self._cv_point_size + 6:
                     if (abs(point_y - mouse_y)) < self._cv_point_size + 6:
                         drag_x_offset = point_x - mouse_x
                         drag_y_offset = point_y - mouse_y
                         self._drag_point = (index, cv_index, (drag_x_offset, drag_y_offset))
                         self._selected_point = (index, cv_index)
+
+            # If still no intersection, check if we clicked a curve
+            mpos_relx = float(mouse_x) / (self.width()-self._legend_border)
+            curve_py = curve.get_value(mpos_relx)
+            curve_offy = self._get_y_value_for(curve_py) - self._bar_h
+
+            if abs(curve_offy - mouse_y) < 8 and self._selected_point is None:
+                # Clicked on curve, spawn new point
+                cv_index = curve.append_cv(mpos_relx, curve_py)
+
+                self._selected_point = (index, cv_index)
+                self._drag_point = (index, cv_index, (0, 0))
 
         self.update()
 
@@ -191,7 +96,6 @@ class CurveWidget(QtGui.QWidget):
         
     def mouseMoveEvent(self, event):
         """ Internal mouse-move handler """
-        # print("mouse moved:", event.pos())
         if self._drag_point is not None:
             mouse_x = event.pos().x() - self._drag_point[2][0] - self._legend_border
             mouse_y = event.pos().y() - self._drag_point[2][1] - self._bar_h
@@ -249,8 +153,6 @@ class CurveWidget(QtGui.QWidget):
         # Draw legend
             
         # Compute amount of horizontal / vertical lines
-        # line_spacing = 20
-
         num_vert_lines = 6 # 24 / 6 = 4, one entry per 4 hours
         line_spacing_x = (self.width() - self._legend_border) / 6.0
         line_spacing_y = (self.height() - self._legend_border - self._bar_h) / 20.0
@@ -287,7 +189,6 @@ class CurveWidget(QtGui.QWidget):
 
 
         # Draw curve
-
         for index, curve in enumerate(self.curves):
             painter.setPen(QtGui.QColor(*curve.get_color()))
             last_value = 0
