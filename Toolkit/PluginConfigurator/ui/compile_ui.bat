c49f338134a82b211ec4604baf68c4ab3935f664
tobspr <tobias.springer1@googlemail.com>
2015-11-24 15:51:14 +0100

    Update UI Compilation scripts

--- Toolkit/PluginConfigurator/ui/compile_ui.bat
+++ Toolkit/PluginConfigurator/ui/compile_ui.bat
@@ -1,11 +1,10 @@
 @echo off
 
 REM Adjust the path to match your PyQt4 installation
-REM SET PYQTPATH="E:\ProgrammeTemp\PyQT\Lib\site-packages\PyQt4"
-SET PYQTPATH="C:\Panda3D-1.10.0-x64\python\Lib\site-packages\PyQt4"
+SET PYQTPATH="E:\ProgrammeTemp\PyQT\Lib\site-packages\PyQt4"
+REM SET PYQTPATH="C:\Projekte\PythonInc\PyQt4"
 
 python "%PYQTPATH%\uic\pyuic.py" main_window.ui -o main_window_generated.py
 "%PYQTPATH%\pyrcc4.exe" resources.qrc -o resources_rc.py
-
 
 pause
