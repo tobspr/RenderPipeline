206842f3789fcc432167b475223dc8a94ad13d42
tobspr <tobias.springer1@googlemail.com>
2016-01-06 18:20:53 +0100

    Fix init script

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
 # This file includes all classes from the pipeline which are exposed
 
+
 import os
 import sys
 import importlib
@@ -11,6 +12,11 @@ __all__ = ["RenderPipeline", "SpotLight", "PointLight"]
 root_pth = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, root_pth)
 
+# Add the six library to the Path for Python 2 and 3 compatiblity. This is handy
+# so we can just do "import six" everywhere.
+sys.path.insert(0, os.path.join(root_pth, "Code/External/six/"))
+print(root_pth)
+
 # Import the classes without a module, otherwise we get problems when trying to
 # import the plugins.
 for module_src, module_name in [
@@ -19,7 +25,3 @@ for module_src, module_name in [
         ("Code.Native", "PointLight")
     ]:
     globals()[module_name] = getattr(importlib.import_module(module_src), module_name)
-
-# Add the six library to the Path for Python 2 and 3 compatiblity. This is handy
-# so we can just do "import six" everywhere.
-sys.path.insert(0, os.path.join(root_pth, "Code/External/six"))
