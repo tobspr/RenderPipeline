6fcfae0c3215573689f307969242d5ff2bfb2d23
wolfgangp <wolfgangpratl@gmail.com>
2016-01-12 22:49:20 +0100

    Compile PyQt4 GUIs in a Python 3 compatible way.

--- Toolkit/PluginConfigurator/ui/compile_ui.bat
+++ Toolkit/PluginConfigurator/ui/compile_ui.bat
@@ -4,7 +4,7 @@ REM Adjust the path to match your PyQt4 installation
 SET PYQTPATH="E:\ProgrammeTemp\PyQT\Lib\site-packages\PyQt4"
 REM SET PYQTPATH="C:\Projekte\PythonInc\PyQt4"
 
-python "%PYQTPATH%\uic\pyuic.py" main_window.ui -o main_window_generated.py
-"%PYQTPATH%\pyrcc4.exe" resources.qrc -o resources_rc.py
+python "%PYQTPATH%\uic\pyuic.py" --from-imports main_window.ui -o main_window_generated.py
+"%PYQTPATH%\pyrcc4.exe" -py3 resources.qrc -o resources_rc.py
 
 pause
