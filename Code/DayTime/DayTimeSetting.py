bca7a34876d705649f2a11b552b994b947f4369c
tobspr <tobias.springer1@googlemail.com>
2016-01-22 19:49:14 +0100

    Update license date

--- Code/DayTime/DayTimeSetting.py
+++ Code/DayTime/DayTimeSetting.py
@@ -2,7 +2,7 @@
 
 RenderPipeline
 
-Copyright (c) 2014-2015 tobspr <tobias.springer1@gmail.com>
+Copyright (c) 2014-2016 tobspr <tobias.springer1@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
